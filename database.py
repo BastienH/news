b8deaefb1b71bfe31ab12cd998430175983e2aec
Bastien <kokoins@hotmail.fr>
2020-02-14 12:37:39 +0100

    Update database.py

--- database.py
+++ database.py
@@ -2,17 +2,22 @@ import json
 from os.path import join, getctime
 import os
 
-def newest(path):
-    path = join(os.getcwd(), path)
-    files = os.listdir(path)
-    paths = [os.path.join(path, basename) for basename in files]
+def newest(path_to_dir):
+    """
+    Returns the most recently created file in a directory
+    """
+    path_to_dir = join(os.getcwd(), path)
+    files = os.listdir(path_to_dir)
+    paths = [os.path.join(path_to_dir, basename) for basename in files]
     return max(paths, key=getctime)
 
 
 def open_recent_data():
-    with open(newest('JSON')) as f:
+    """
+    Returns the most recent data and header
+    """
+    with open(newest('JSON'), encoding='utf-8') as f:
         data = json.loads(f.read())
         header = data['header']
         data.pop('header', None)
-
     return (data, header)
